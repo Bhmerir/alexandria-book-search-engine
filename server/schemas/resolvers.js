a129880d16aeedc7dbde9100eea4fd82b72cd9b0
Bahareh <mer_ir@yahoo.com>
2023-06-02 15:36:31 -0700

    fix the error of the query of me

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,9 @@ const resolvers = {
     me: async (parent, args, context) => {
       console.log(context.user)
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('saveBooks');
+        const foundUser = User.findOne({ _id: context.user._id }).populate('savedBooks');
+        console.log("foundUser",foundUser)
+        return foundUser
       }
       throw new AuthenticationError('You need to be logged in!');
     },
